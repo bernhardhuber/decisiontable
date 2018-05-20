79d1f1047e7ad5242a1e611c49a3716ad08de110
Bernhard Huber <bernihuber123@gmail.com>
2018-05-20 09:51:40 +0200

    add unit-test testing Supplier

--- src/test/java/org/huberb/decisiontable/ConditionsTest.java
+++ src/test/java/org/huberb/decisiontable/ConditionsTest.java
@@ -6,8 +6,10 @@
 package org.huberb.decisiontable;
 
 import java.util.List;
+import java.util.function.Predicate;
 import org.huberb.decisiontable.Conditions.ConditionResult;
 import org.huberb.decisiontable.Person.ConditionsEnum;
+import org.huberb.decisiontable.tuple.Couple;
 import org.junit.After;
 import static org.junit.Assert.assertEquals;
 import org.junit.Before;
@@ -19,17 +21,6 @@ import org.junit.Test;
  */
 public class ConditionsTest {
 
-    public ConditionsTest() {
-    }
-
-    @Before
-    public void setUp() {
-    }
-
-    @After
-    public void tearDown() {
-    }
-
     @Test
     public void test_DecisionTableConditions() {
         final List<Person> persons = Person.tenPersons();
@@ -37,6 +28,30 @@ public class ConditionsTest {
                 new ConditionsListBuilder<Person>().
                         enumPredicate(ConditionsEnum.c1, (p) -> p.age > 20).
                         enumPredicate(ConditionsEnum.c2, (p) -> p.weight % 2 == 0).
+                        build()
+        );
+        final ConditionResult conditionResult = dtConditionsPerson.evaluateUsing(persons.get(0));
+
+        assertEquals(2, conditionResult.result().size());
+
+        assertEquals(ConditionsEnum.c1, conditionResult.result().get(0).getT());
+        assertEquals(false, conditionResult.result().get(0).getU());
+
+        assertEquals(ConditionsEnum.c2, conditionResult.result().get(1).getT());
+        assertEquals(true, conditionResult.result().get(1).getU());
+
+        assertEquals(false, dtConditionsPerson.findByCondtionEnum(conditionResult, ConditionsEnum.c1).get());
+        assertEquals(true, dtConditionsPerson.findByCondtionEnum(conditionResult, ConditionsEnum.c2).get());
+    }
+
+    @Test
+    public void test_DecisionTableConditions_Supplier() {
+        final List<Person> persons = Person.tenPersons();
+
+        final Conditions<Person> dtConditionsPerson = new Conditions<Person>(
+                new ConditionsListBuilder<Person>().
+                        enumPredicateSupplier(() -> new Couple<>(ConditionsEnum.c1, (p) -> p.age > 20)).
+                        enumPredicateSupplier(() -> new Couple<>(ConditionsEnum.c2, (p) -> p.weight % 2 == 0)).
                         build()
         );
         final ConditionResult conditionResult = dtConditionsPerson.evaluateUsing(persons.get(0));
